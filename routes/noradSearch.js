705407f78406eb43d3e6fd479fec615dbfaeb793
Jon Pitans <jonpitans@gmail.com>
2023-12-09 21:00:38 +0000

    added sat details to norad route

--- routes/noradSearch.js
+++ routes/noradSearch.js
@@ -7,9 +7,52 @@ app.get("/:id", async (req, res) => {
   const id = req.params.id;
   const validationCheck = await noradValidate(id);
   if (id && validationCheck) {
-    const findSat = await asyncMySQL(
-      `SELECT Name as name, TLE as tle, Norad as noradId FROM active WHERE Norad=${id}`
+    //check details table for cached details, if not call uphere.space api
+    // to get sat details and write to SQL details table
+    const detailQuery = await asyncMySQL(
+      `SELECT NoradId FROM details WHERE NoradId=${id}`
     );
+    if (detailQuery.length === 0) {
+      console.log("API CALLED");
+      const options = {
+        method: "GET",
+        url: `https://uphere-space1.p.rapidapi.com/satellite/${id}/details`,
+        headers: {
+          "x-rapidapi-host": "uphere-space1.p.rapidapi.com",
+          "x-rapidapi-key":
+            "fd0e7fa6f4msh9c28dde032e2e80p10deb9jsn913fa371e71a",
+        },
+      };
+      const details = await axios.request(options); //from uphere docs
+      const {
+        type,
+        country,
+        intldes,
+        orbital_period,
+        launch_date,
+        description,
+        links,
+      } = details.data;
+
+      await asyncMySQL(
+        `INSERT INTO details (NoradId, type, country, intldes, orbital_period, launch_date, description, links) VALUES (?,?,?,?,?,?,?,?)`,
+        [
+          id,
+          type,
+          country,
+          intldes,
+          orbital_period,
+          launch_date,
+          description,
+          JSON.stringify(links),
+        ]
+      );
+    }
+    console.log(detailQuery);
+    const findSat = await asyncMySQL(
+      `SELECT Name as name, TLE as tle, Norad as noradId, details.type, details.country, details.intldes, details.orbital_period, details.launch_date, details.links, details.description FROM active LEFT JOIN details ON active.Norad = details.NoradID WHERE Norad=${id}`
+    );
+
     res.status(200).send(findSat);
   } else {
     res.status(400).send("invalid Norad ID submitted");
